563c25953b3ed798377675bd5b4e1e1c738ee42d
Sam.Lin <greenlin111@gmail.com>
2018-02-26 15:41:26 +0800

    basic html oauth 2 provider

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :access_grants, dependent: :delete_all
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:sso]
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # Easier to make GET request from the client
+  devise_for :users, sign_out_via: [:get, :delete],
+              controllers: { omniauth_callbacks: 'callbacks' }
+
+  root 'home#index'
+
+  # Provider stuff
+  match '/auth/sso/authorize' => 'auth#authorize', via: :all
+  match '/auth/sso/access_token' => 'auth#access_token', via: :all
+  match '/auth/sso/user' => 'auth#user', via: :all
+  match '/oauth/token' => 'auth#access_token', via: :all
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
